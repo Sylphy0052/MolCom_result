c62e1cf68abae0b7020367804899f93c1636af76
Sylphy0052 <ma17099@shibaura-it.ac.jp>
2018-04-25 16:25:46 +0900

    update thesis figures of comparing fec and fec-swarq

---
+++ thesis_result/analyzer4/remove_result.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -r compare* regression* result_img adjust_graph collision_graph retransmission_graph >& /dev/null
+rm result/*.png >& /dev/null
+rm result/time.txt >& /dev/null
+rm index.html >& /dev/null
+rm pickle* >& /dev/null
+rm csv_data.txt data.csv >& /dev/null
